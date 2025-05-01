589a88dc08a02ebcc0380e899184fda769b23c07
soujanya8 <soujanyagodugu8@gmail.com>
2025-05-01 13:27:46 -0700

    java Script Basics

---
+++ A_basics/index.js
@@ -0,0 +1,7 @@
+//console.log(`hello`);
+//window.alert(`Alert`);
+/*hello
+   hdgjh */
+
+//document.getElementById("id1").textContent = 'hello`;
+//document.getElementByClass("myclass").textContent = `hello`;
