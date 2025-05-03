7d68e9d24c329bba9231a0a0022a31f92efc7ea7
soujanya8 <soujanyagodugu8@gmail.com>
2025-05-03 13:38:11 -0700

    arithmetic operators

---
+++ C_Arithmetic operators/index.js
@@ -0,0 +1,31 @@
+ /*Arithmetic Operators and it precedence
+                         operands ex: values, varaiable etc
+                         operators + - * /  
+                        */
+
+
+/*
+let students =30;
+students= students + 1;
+//agumented assignment operators
+students += 1;
+//increment operator //ly decrement
+students ++;
+console.log(students);
+*/
+
+
+// operator precedence
+/*
+ Parenthesis
+ Exponents
+ * ,/,%;
+ +,-
+ */
+
+ //let result = 1 + 2*3/6 +1;
+ let result = (1 + 2)*3/(6 +1);
+ console.log(result);
+
+
+
