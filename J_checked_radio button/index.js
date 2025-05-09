d84ba3eb40c299306a89915f367f7bad31d39406
soujanya8 <soujanyagodugu8@gmail.com>
2025-05-09 14:17:38 -0700

    checked and radio buttons

---
+++ J_checked_radio button/index.js
@@ -0,0 +1,8 @@
+const subBtn = document.getElementById("subBtn");
+const visaBtn = document.getElementById("visaBtn");
+const giftBtn = document.getElementById("giftBtn");
+
+subBtn.onclick = function(){
+   
+
+}
